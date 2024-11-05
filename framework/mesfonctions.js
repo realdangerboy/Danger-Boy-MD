73317fd60797ead8a5d8c5abce344b6dd5d100fc
Danger Boy <148145155+Danger-Boy-MD@users.noreply.github.com>
2024-11-05 14:10:50 +0500

    Update mesfonctions.js

--- framework/mesfonctions.js
+++ framework/mesfonctions.js
@@ -33,7 +33,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'Zokou-MD',
+        pack: '⎝⎝ᴰᴬᴺᴳᴱᴿ𖡻ʙᴏʏ𖧹ᴹᴰ⎠⎠',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
